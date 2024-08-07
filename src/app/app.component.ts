b15fee85abe4c96b74dcf6e3bd05a5ab6a1c8cfc
katie360 <kateratemo44@gmail.com>
2024-08-07 13:25:46 +0300

    Complete challange 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  randomString = signal('Angular Kenya!');
+  randomString = 'Angular Kenya!';
 }
